afefa2d8d54681083e6e5a9e94cdb857d59855eb
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-05-18 02:06:38 +0530

    made a basic landing page

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,8 +1,13 @@
-
+import { RoughNotation } from "react-rough-notation"
 function Landing() {
   return (
     <div className="overflow-x-hidden">
-<div className="bg-black w-screen h-screen">
+<div className="bg-white w-screen h-screen">
+  <div className="bg-white h-full w-full flex justify-center items-center">
+    <RoughNotation type="underline" show={true} color="#6700c9" strokeWidth={3}>
+    <p style={{ fontFamily: '"Gochi Hand",cursive' , marginBottom: '-0.3em'}} className="text-8xl">Sketchy</p>
+</RoughNotation>
+  </div>
 </div>
 <div className="bg-white h-screen w-screen">
 </div>

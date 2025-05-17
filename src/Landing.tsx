f361adb6df2cb578cdcd06abd5a45df1388d3275
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-05-18 00:46:54 +0530

    prim page

---
+++ src/Landing.tsx
@@ -0,0 +1,14 @@
+
+function Landing() {
+  return (
+    <div className="overflow-x-hidden">
+<div className="bg-black w-screen h-screen">
+</div>
+<div className="bg-white h-screen w-screen">
+</div>
+</div>
+
+  )
+}
+
+export default Landing

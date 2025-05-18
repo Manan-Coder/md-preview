eb5abf542135fe1968072efca33091252665f041
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-05-18 23:48:06 +0530

    did some md rendering setup work

--- src/components/output.tsx
+++ src/components/output.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 const Output = () => {
-  const markdown = `# Hey
-hello
-`;
+  const [markdown, setMarkdown] = useState(`# Heading
+
+*Italic*
+
+**Bold**
+
+~~Strikethrough~~
+
+> Blockquote
+
+\`\`\`js
+console.log("code block");
+\`\`\`
+`);
 
   return (
-    <div className="flex bg-white text-black font-mono text-sm h-[500px] w-[800px] rounded overflow-auto p-4 border-2">
-      <Markdown>{markdown}</Markdown>
+    <div className="flex flex-col bg-white text-black h-full w-full rounded overflow-auto p-4 border-2">
+      <div className="prose prose-slate prose-headings:font-bold prose-blockquote:font-normal prose-blockquote:border-l-4 prose-blockquote:border-gray-300 prose-blockquote:pl-4 prose-blockquote:italic prose-code:bg-gray-100 prose-code:px-1 prose-code:py-0.5 prose-code:rounded prose-pre:bg-gray-100 prose-pre:p-4 prose-pre:rounded max-w-none">
+        <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
+      </div>
     </div>
   );
 };
